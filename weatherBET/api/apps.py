0b976aa49b8b499e7d3812a96c1a71406e8a7893
Mikołaj <266518@student.pwr.edu.pl>
2023-06-14 19:53:37 +0200

    first working map

--- weatherBET/api/apps.py
+++ weatherBET/api/apps.py
@@ -1,6 +1,13 @@
 from django.apps import AppConfig
+from .scheduler import Command
 
 
 class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
+
+    def ready(self): # Import Twojego polecenia
+        from api.models import CityWeather
+        Command().handle()  # Uruchom Twoje polecenie
+
+
--- weatherBET/api/models.py
+++ weatherBET/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class CityWeather(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    was_rain = models.BooleanField()
+    forecast_mm = models.FloatField()
+    precip_mm = models.FloatField()
+    rain_chance = models.IntegerField()
+    yes_course = models.FloatField()
+    no_course = models.FloatField()
